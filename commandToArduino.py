e898af52492bcb4207e5fab385f3707713eeab80
PolleAnker <thementoryay@gmail.com>
2019-04-30 11:16:56 +0200

    changed the command sending to take numbers as well for commands on the x, o, square and triangle side

--- commandToArduino.py
+++ commandToArduino.py
@@ -8,32 +8,35 @@ def sendCommand(command, serialPort):
         '''
 
         if(command == "left"):
-            serialPort.write(b'1')
-            print("Sent '1' to Arduino (left)")
-        elif(command == "right"):
-            serialPort.write(b'2')
-            print("Sent '2' to Arduino (right)")
-        elif(command == "up"):
             serialPort.write(b'0')
-            print("Sent '0' to Arduino (up)")
+            print("Sent '0' to Arduino (left)")
+        elif(command == "right"):
+            serialPort.write(b'1')
+            print("Sent '1' to Arduino (right)")
+        elif(command == "up"):
+            serialPort.write(b'2')
+            print("Sent '2' to Arduino (up)")
         elif(command == "down"):
             serialPort.write(b'3')
             print("Sent '3' to Arduino (down)")
-        elif(command == "square"):
+        elif(command == "two"):
             serialPort.write(b'4')
-            print("Sent '5' to Arduino (square)")
-        elif (command == "tri"):
+            print("Sent '5' to Arduino (square / two)")
+        elif (command == "three"):
             serialPort.write(b'5')
-            print("Sent '4' to Arduino (tri)")
-        elif(command == "round"):
-            serialPort.write(b'7')
-            print("Sent '7' to Arduino (round)")
-        elif(command == "cross"):
+            print("Sent '4' to Arduino (tri / three)")
+        elif(command == "four"):
             serialPort.write(b'6')
-            print("Sent '6' to Arduino (cross)")
+            print("Sent '6' to Arduino (round / four)")
+        elif(command == "one"):
+            serialPort.write(b'7')
+            print("Sent '7' to Arduino (cross / one)")
         elif(command == "stop"):
+            serialPort.write(b'8')
+            print("Sent '8' to Arduino (stop)")
+        elif(command == "go"):
             serialPort.write(b'9')
-            print("Sent '9' to Arduino (stop)")
+            print("Sent '9' to Arduino (go)")
         else:
-            serialPort.write(b'10')
+            serialPort.write(b'99')
             print("Command not recognized (_unknown_)")
